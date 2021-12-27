815d1300bccec4dc4e10f967063fb3ef62201171
gladius <gladiusservices@protonmail.com>
2021-12-27 16:16:11 +0100

    Update start script to check server status and kill server screen

--- commands/start.sh
+++ commands/start.sh
@@ -2,12 +2,16 @@
 
 serverDirPath=$1
 
-echo "$serverDirPath"
+serverProcess=$(pgrep -f start-server)
+[ -n "$serverProcess" ] && echo "Server is already up and running" && exit
+
+# kill server screen
+screen -ls | grep server | cut -d. -f1 | awk '{print $1}' | xargs kill
 
 echo "Creating server screen"
 screen -S server
 
-cd "$serverDirPath" || echo "Can't cd into server folder. Abort." && exit
+cd "$serverDirPath" || (echo "Can't cd into server folder. Abort." && exit)
 
 echo "Starting server"
 ./start-server.sh
