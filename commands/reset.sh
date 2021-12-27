01d8350c23d231a0b57d243faa43ccaa45656ea8
gladius <gladiusservices@protonmail.com>
2021-12-27 16:46:18 +0100

    Create reset script

---
+++ commands/reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+databasePath=1$
+savesPath=2$
+
+serverProcess=$(pgrep -f start-server)
+[ -n "$serverProcess" ] && echo "Server is running. Can't wipe" && exit
+
+echo "Are you sure? y/N"
+read -r confirm
+
+if [ -z "$confirm" ] || [ "$confirm" == "n" ] || [ "$confirm" == "N" ]
+then
+  echo "Reset aborted"
+  exit
+fi
+
+echo "Deleting database"
+rm $databasePath
+
+echo "Deleting saves"
+rm -rf $savesPath
+
+echo "Server wiped"
