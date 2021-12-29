5ad1e6523c16cce1d246fab8738a98d2e53e2a35
gladius <gladiusservices@protonmail.com>
2021-12-29 13:33:00 +0100

    Update zombob script with code quality enhancements

--- zombob.sh
+++ zombob.sh
@@ -10,9 +10,9 @@ MODS_CONFIG_PATH="${WORKSHOP_PATH}/appworkshop_108600.acf"
 MODS_PATH="${WORKSHOP_PATH}/content/108600"
 
 ZOMBOID_PATH="${HOME}/Zomboid"
-databasePath="${ZOMBOID_PATH}/db/servertest.db"
-savesPath="${ZOMBOB_PATH}/Saves/Multiplayer/servertest"
-backupsPath="${ZOMBOID_PATH}/Backups"
+DATABASE_PATH="${ZOMBOID_PATH}/db/servertest.db"
+SAVES_PATH="${ZOMBOID_PATH}/Saves/Multiplayer/servertest"
+BACKUPS_PATH="${ZOMBOID_PATH}/Backups"
 
 # input handler
 actionSelector() {
@@ -65,7 +65,7 @@ restart() {
 # reset server
 reset() {
   echo "[ZOMBOB] > reset"
-  $COMMANDS_PATH/reset.sh $databasePath $savesPath
+  $COMMANDS_PATH/reset.sh $DATABASE_PATH $SAVES_PATH
 }
 
 # update server
@@ -76,7 +76,7 @@ update() {
 # create backup
 createBackup() {
   echo "[ZOMBOB] > create backup"
-  $COMMANDS_PATH/create-backup.sh $savesPath $databasePath $backupsPath
+  $COMMANDS_PATH/create-backup.sh $SAVES_PATH $DATABASE_PATH $BACKUPS_PATH
 }
 
 # restore backup
@@ -87,7 +87,7 @@ restoreBackup() {
 # update mods
 updateMods() {
   echo "[ZOMBOB] > update mods"
-  $COMMANDS_PATH/update-mods.sh $MODS_CONFIG_PATH $MODS_PATH $savesPath $databasePath $backupsPath $SERVER_PATH
+  $COMMANDS_PATH/update-mods.sh $MODS_CONFIG_PATH $MODS_PATH $SAVES_PATH $DATABASE_PATH $BACKUPS_PATH $SERVER_PATH
 }
 
 echo "Please input an action {start|stop|restart|reset|update|create-backup|restore-backup|update-mods}"
