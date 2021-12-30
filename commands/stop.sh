6c1139b7e64ba745fc3294f19db5faafe5059cf0
gladius <gladiusservices@protonmail.com>
2021-12-30 12:52:45 +0100

    Update stop script with code quality enhancements and way to avoid waiting

--- commands/stop.sh
+++ commands/stop.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 
+serverProcess=$(pgrep -f start-server)
+[ -z "$serverProcess" ] && echo "Server is already down" && exit 0
+
+SHOULD_WAIT_AND_SAVE=$1
+
 executeCommand() {
   screen -S server -X stuff "$1^M"
 }
 
-quitMessage="servermsg Il-server-si-spegnerà-tra-"
+save() {
+  echo "Saving current world"
+  executeCommand "save"
+  sleep 10s
+  echo "Current world saved"
+}
 
-serverProcess=$(pgrep -f start-server)
-[ -z "$serverProcess" ] && echo "Server is already down" && exit
+quit() {
+  echo "Stopping server"
+  executeCommand "quit"
+  sleep 5s
+  echo "Server stopped"
+}
 
-echo "Waiting 5 minutes..."
-executeCommand "${quitMessage}5-minuti"
-sleep 5m
+if [ -z "$SHOULD_WAIT_AND_SAVE" ] || [ "$SHOULD_WAIT_AND_SAVE" == "Y" ] || [ "$SHOULD_WAIT_AND_SAVE" == "y" ]
+then
+  QUIT_MESSAGE="servermsg Il-server-si-spegnerà-tra-"
 
-echo "Saving current world"
-executeCommand "save"
-sleep 10s
-echo "Current world saved"
+  echo "Waiting 5 minutes..."
+  executeCommand "${QUIT_MESSAGE}5-minuti"
+  sleep 5m
 
-executeCommand "${quitMessage}5-secondi"
-sleep 1s
-executeCommand "${quitMessage}4-secondi"
-sleep 1s
-executeCommand "${quitMessage}3-secondi"
-sleep 1s
-executeCommand "${quitMessage}2-secondi"
-sleep 1s
-executeCommand "${quitMessage}1-secondo"
-sleep 1s
+  save
 
-echo "Stopping server"
-executeCommand "quit"
-sleep 5s
-echo "Server stopped"
+  executeCommand "${QUIT_MESSAGE}5-secondi"
+  sleep 1s
+  executeCommand "${QUIT_MESSAGE}4-secondi"
+  sleep 1s
+  executeCommand "${QUIT_MESSAGE}3-secondi"
+  sleep 1s
+  executeCommand "${QUIT_MESSAGE}2-secondi"
+  sleep 1s
+  executeCommand "${QUIT_MESSAGE}1-secondo"
+  sleep 1s
+
+  quit
+
+  exit 1
+fi
+
+save
+
+quit
 
 screen -ls | grep server | cut -d. -f1 | awk '{print $1}' | xargs kill
 
-
+exit 1
