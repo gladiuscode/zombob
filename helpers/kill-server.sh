b053841ebcc6b07772aa0fa67db42a9b84b465e9
gladius <gladiusservices@protonmail.com>
2022-01-23 17:46:26 +0100

    Fix kill-server logic

--- helpers/kill-server.sh
+++ helpers/kill-server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 killServer() {
-  PROCESS=$( pgrep -f "./start-server.sh -servername $SERVER_NAME" )
+  SCREEN_PROCESS=$( pgrep -f "SCREEN -S $SERVER_NAME" )
 
-  if [ -z "$PROCESS" ]
+  if [ -z "$SCREEN_PROCESS" ]
   then
     echo "[ ZOMBOB : INFO ] > There is no server screen to kill"
     return
   fi
 
   echo "[ ZOMBOB : INFO ] > Killing server screen"
-  echo "$PROCESS" | cut -d. -f1 | awk '{print $1}' | xargs kill
+  echo "$SCREEN_PROCESS" | cut -d. -f1 | awk '{print $1}' | xargs kill
 }
