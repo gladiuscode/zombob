9b397cec3a82045cba58707faced27d3b50c1fd7
gladius <gladiusservices@protonmail.com>
2022-01-23 17:41:10 +0100

    Update kill-server.sh script to enhance readability and code quality

--- helpers/kill-server.sh
+++ helpers/kill-server.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 killServer() {
-  echo "Killing screen and server"
-  screen -ls | grep "$SCREEN_SERVER_NAME" | cut -d. -f1 | awk '{print $1}' | xargs kill
+  PROCESS=$( pgrep -f "./start-server.sh -servername $SERVER_NAME" )
+
+  if [ -z "$PROCESS" ]
+  then
+    echo "[ ZOMBOB : INFO ] > There is no server screen to kill"
+    return
+  fi
+
+  echo "[ ZOMBOB : INFO ] > Killing server screen"
+  echo "$PROCESS" | cut -d. -f1 | awk '{print $1}' | xargs kill
 }
