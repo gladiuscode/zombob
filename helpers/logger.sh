e1b49ec6a8cdb426f4d82af0d7c9013bc8b14f75
gladius <gladiusservices@protonmail.com>
2022-02-04 00:02:42 +0100

    Fix logger script

--- helpers/logger.sh
+++ helpers/logger.sh
@@ -4,7 +4,7 @@ logger() {
   INFO="[ ZOMBOB : INFO ] > "
   ERROR="[ ZOMBOB : ERROR ] > "
 
-  if [ "$AGENT" != "BOT" ]
+  if [ "$AGENT" != "BOT" ] && [ "$1" != '-s' ]
   then
     [ "$1" == "-e" ] && echo "$ERROR $2" || echo "$INFO $1"
     return;
