5fe6a6b1687a4744d8531871def6841e3bcb9551
gladius <gladiusservices@protonmail.com>
2022-02-04 15:16:43 +0100

    Update count-players output

--- commands/count-players.sh
+++ commands/count-players.sh
@@ -8,11 +8,16 @@ countPlayers() {
   sendServerCommand "players"
   sleep 10s
 
-  PLAYERS=$(grep -o "Players.*" "$SERVER_CONSOLE" | tail -1)
+  RAW_PLAYERS=$(grep -o "Players.*" "$SERVER_CONSOLE" | tail -1)
+  RAW_MAX_PLAYERS=$(grep -o "MaxPlayers=.*" "$SERVER_INI_PATH")
+  PLAYERS=${RAW_PLAYERS//[^0-9]/}
+  MAX_PLAYERS=${RAW_MAX_PLAYERS//[^0-9]/}
 
-  logger "[ ZOMBOB : INFO ] > $PLAYERS"
+  MESSAGE="$PLAYERS / $MAX_PLAYERS"
+
+  logger "[ ZOMBOB : INFO ] > $MESSAGE"
 
   logger "[ ZOMBOB : INFO ] > Count Players stopped"
 
-  logger -s "$PLAYERS"
+  logger -s "$MESSAGE"
 }
