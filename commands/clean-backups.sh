0ff3f3eba862650c159fa4fa7463fb626a088726
gladius <gladiusservices@protonmail.com>
2022-02-08 16:07:09 +0100

    Update and fix clean-backups script

--- commands/clean-backups.sh
+++ commands/clean-backups.sh
@@ -3,7 +3,7 @@
 cleanBackups() {
   logger "Server cleanup backups started"
 
-  find "$ZOMBOB_DATA_BACKUPS_PATH" -maxdepth 1 -type d -mtime +"$ZOMBOB_DATA_BACKUPS_CLEANUP_DAYS" -exec rm -rf {} \;
+  find "$ZOMBOB_DATA_BACKUPS_PATH" -mtime +1 -exec rm -rf {} \;
 
   logger "Server cleanup backups completed"
 }
