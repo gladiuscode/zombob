13f5f941a43c533bf1323aaaffa66ce63b7d07b4
gladius <gladiusservices@protonmail.com>
2022-02-28 20:10:11 +0100

    FEATURE PRE-RELEASE: Improve create-backup code quality, logging and update logic to include zomboid server folder too.

--- commands/create-backup.sh
+++ commands/create-backup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 createBackup() {
-  logger "Server backup started"
+  logger "[ CREATE_BACKUP ] START"
 
+  local RAW_BACKUP_DATE
   RAW_BACKUP_DATE=$( date +"%c" )
-  BACKUP_DATE="${RAW_BACKUP_DATE// /_}"
 
-  BACKUP_FOLDER="$ZOMBOB_DATA_BACKUPS_PATH/$BACKUP_DATE"
-  BACKUP_FOLDER_NAME="$ZOMBOB_DATA_BACKUPS_PATH/$BACKUP_DATE/backup.tar"
+  local BACKUP_DATE="${RAW_BACKUP_DATE// /_}"
+  local BACKUP_FOLDER="$ZOMBOB_DATA_BACKUPS_PATH/$BACKUP_DATE"
+  local BACKUP_FOLDER_NAME="$ZOMBOB_DATA_BACKUPS_PATH/$BACKUP_DATE/backup.tar"
 
   [ ! -d "$ZOMBOB_DATA_BACKUPS_PATH" ] && mkdir -p "$ZOMBOB_DATA_BACKUPS_PATH"
   [ ! -d "$ZOMBOB_DATA_BACKUPS_PATH" ] && logger "Backups directory in Zomboid folder is missing. Please create it." && exit 1
 
   logger "Creating backup"
   mkdir "$BACKUP_FOLDER"
-  tar -zcf "$BACKUP_FOLDER_NAME" "$SAVES_PATH" "$DATABASE_PATH"
+  tar -zcf "$BACKUP_FOLDER_NAME" "$SAVES_PATH" "$DATABASE_PATH" "$ZOMBOID_SERVER_FOLDER_PATH"
   logger "Backup created"
 
-  logger "Server backup completed"
+  logger "Backup done"
+  logger "[ CREATE_BACKUP ] END"
 }
