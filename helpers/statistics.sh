4055a5014e6bdeff9b494722df32e660a1fef6f9
gladius <gladiusservices@protonmail.com>
2022-02-28 21:14:17 +0100

    FEATURE PRE-RELEASE: Improve statistics script logging

--- helpers/statistics.sh
+++ helpers/statistics.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
 trackPlayers() {
+  logger "[ TRACK_PLAYERS : HELPER ] START"
+
+  local NOW
   NOW=$( date +"%c" )
+
   echo "$NOW" >> "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
   echo "[Players] $PLAYERS_COUNT" >> "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
+
+  logger "[ TRACK_PLAYERS : HELPER ] END"
 }
 
 cleanUpPlayersStatistics() {
+  logger "[ CLEAN_UP_PLAYERS_STATS : HELPER ] START"
+
   if test -f "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
   then
     rm "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
   fi
+
+  logger "[ CLEAN_UP_PLAYERS_STATS : HELPER ] END"
 }
 
 trackStatus() {
+  logger "[ TRACK_STATUS : HELPER ] START"
+
   cleanUpStatusStatistics
-  STATUS=$1
+
+  local STATUS=$1
   echo "[Status] $STATUS" >> "$ZOMBOB_DATA_STATUS_FILE_PATH"
+
+  logger "[ TRACK_STATUS : HELPER ] END"
 }
 
 cleanUpStatusStatistics() {
+  logger "[ CLEAN_UP_STATUS_STATS : HELPER ] START"
+
   if test -f "$ZOMBOB_DATA_STATUS_FILE_PATH"
   then
     rm "$ZOMBOB_DATA_STATUS_FILE_PATH"
   fi
+
+  logger "[ CLEAN_UP_STATUS_STATS : HELPER ] END"
 }
