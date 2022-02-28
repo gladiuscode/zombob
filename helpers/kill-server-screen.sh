79f4e88c31fb0897e27c261b0eaca86c4de053b3
gladius <gladiusservices@protonmail.com>
2022-02-28 21:09:42 +0100

    FEATURE PRE-RELEASE: Improve killServerScreen script logging

--- helpers/kill-server-screen.sh
+++ helpers/kill-server-screen.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 killServerScreen() {
+  logger "[ KILL_SERVER_SCREEN : HELPER ] START"
+  local SCREEN_PROCESS
   SCREEN_PROCESS=$( pgrep -f "SCREEN.*$SERVER_NAME" )
 
   if [ -z "$SCREEN_PROCESS" ]
   then
     logger "There is no server screen to kill"
+    logger "[ KILL_SERVER_SCREEN : HELPER ] END"
     return
   fi
 
-  logger "Killing server screen"
   echo "$SCREEN_PROCESS" | cut -d. -f1 | awk '{print $1}' | xargs kill
+
+  logger "Server screen killed"
+  logger "[ KILL_SERVER_SCREEN : HELPER ] END"
 }
