8841ea5e084639214d29480c922d06e894d69aaf
gladius <gladiusservices@protonmail.com>
2022-02-28 21:12:01 +0100

    FEATURE PRE-RELEASE: Improve startup-date script logging

--- helpers/startup-date.sh
+++ helpers/startup-date.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 setStartupDate() {
+  logger "[ SET_STARTUP_DATE : HELPER ] START"
   STARTUP_DATE=$( date +"%s" )
   touch "$ZOMBOB_DATA_ENV_FILE_PATH"
   echo "$STARTUP_DATE" > "$ZOMBOB_DATA_ENV_FILE_PATH"
+  logger "[ SET_STARTUP_DATE : HELPER ] END"
 }
 
 resetStartupDate() {
+  logger "[ RESET_STARTUP_DATE : HELPER ] START"
   if test -f "$ZOMBOB_DATA_ENV_FILE_PATH"; then
+      logger "Server env file deleted"
       rm "$ZOMBOB_DATA_ENV_FILE_PATH"
   fi
+  logger "[ RESET_STARTUP_DATE : HELPER ] END"
 }
