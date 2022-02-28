be379139dadacd649ca7af537ba8d01f7941ac67
gladius <gladiusservices@protonmail.com>
2022-02-28 21:10:44 +0100

    FEATURE PRE-RELEASE: Improve send-server-stuff script logging

--- helpers/send-server-stuff.sh
+++ helpers/send-server-stuff.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 sendServerCommand() {
+  logger "[ SEND_SERVER_COMMAND ] START"
+
   screen -S "$SCREEN_SERVER_NAME" -X stuff "$1^M"
+
+  logger "[ SEND_SERVER_MESSAGE ] END"
 }
 
 sendServerMessage() {
-  MESSAGE="servermsg \"$1\""
+  logger "[ SEND_SERVER_MESSAGE ] START"
+
+  local MESSAGE="servermsg \"$1\""
   sendServerCommand "$MESSAGE"
+
+  logger "[ SEND_SERVER_MESSAGE ] END"
 }
