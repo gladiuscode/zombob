7119b1337f3b12a6cc99f242f77614dc98e9bd58
gladius <gladiusservices@protonmail.com>
2022-02-28 21:08:37 +0100

    FEATURE PRE-RELEASE: Improve getServerStatus script logging

--- helpers/get-server-status.sh
+++ helpers/get-server-status.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 getServerStatus() {
+  logger "[ GET_SERVER_STATUS : HELPER ] START"
   SERVER_PROCESS=$(pgrep -f "./start-server.sh -servername $SERVER_NAME")
+  logger "[ GET_SERVER_STATUS : HELPER ] END"
 }
