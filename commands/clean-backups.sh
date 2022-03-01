dbed1340a4300b839f860b5d79ceabc270265db4
gladius <gladiusservices@protonmail.com>
2022-03-01 17:39:03 +0100

    FEATURE PRE-RELEASE: Improve clean-backups script logging and try fix logic

--- commands/clean-backups.sh
+++ commands/clean-backups.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cleanBackups() {
-  logger "Server cleanup backups started"
+  logger "[ CLEAN_BACKUPS ] START"
 
-  find "$ZOMBOB_DATA_BACKUPS_PATH" -mtime +1 -exec rm -rf {} \;
+  find "$ZOMBOB_DATA_BACKUPS_PATH" -type d -mtime +1 -exec rm -rf {} \;
 
-  logger "Server cleanup backups completed"
+  logger "Backups clean completed"
+  logger "[ CLEAN_BACKUPS ] END"
 }
