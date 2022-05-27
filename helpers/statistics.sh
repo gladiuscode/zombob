f85a4a5a7d8bb957c8320c3490e3764b4e215bc2
gladius <gladiusservices@protonmail.com>
2022-05-27 19:37:02 +0200

    FEATURE MULTISERVER: Update startup-date and statistics scripts to meet new multiserver requirements

--- helpers/statistics.sh
+++ helpers/statistics.sh
@@ -6,7 +6,7 @@ trackPlayers() {
   local NOW
   NOW=$( date +"%c" )
 
-  echo "$NOW" >> "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
+  echo "$SERVERN_NAME - $NOW" >> "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
   echo "[Players] $PLAYERS_COUNT" >> "$ZOMBOB_DATA_PLAYERS_FILE_PATH"
 
   logger "[ TRACK_PLAYERS : HELPER ] END"
@@ -29,7 +29,7 @@ trackStatus() {
   cleanUpStatusStatistics
 
   local STATUS=$1
-  echo "[Status] $STATUS" >> "$ZOMBOB_DATA_STATUS_FILE_PATH"
+  echo "$SERVER_NAME - [Status] $STATUS" >> "$ZOMBOB_DATA_STATUS_FILE_PATH"
 
   logger "[ TRACK_STATUS : HELPER ] END"
 }
