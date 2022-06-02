e5e3f3ac3983cf50df8172580e049e82da41689a
gladius <gladiusservices@protonmail.com>
2022-06-02 10:21:59 +0200

    FEATURE MULTISERVER: Fix update-config and update zombob

--- helpers/update-config.sh
+++ helpers/update-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 updateConfig() {
-  local REPLACE_STRING="SERVER_NAME=\"servertest\""
-  local WITH_STRING="SERVER_NAME=\"${SERVER_NAME}\""
+  local REPLACE_STRING="SERVER_NAME=\"${SERVER_NAME}\""
+  local WITH_STRING="SERVER_NAME=\"${CUSTOM_SERVER_NAME}\""
 
   sed -i "s/$REPLACE_STRING/$WITH_STRING/" "$CONFIGURATION_PATH"
 
-  echo "Operating on: $SERVER_NAME"
+  echo "Operating on: $CUSTOM_SERVER_NAME"
 }
